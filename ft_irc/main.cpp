24885b98546ec5f250ef7438398a9540134b0d68
Daeyoung Choi <daechoi@c10r5s8.42seoul.kr>
2023-05-01 18:48:57 +0900

    prev version

--- ft_irc/main.cpp
+++ ft_irc/main.cpp
@@ -8,70 +8,6 @@ void print_map(map<string, Client> m) {
 		cout << "{" << (*it).first << "}\n";
 }
 
-// int main(int ac, char **av)
-// {
-// 	if (ac != 3) 
-// 		perr("Usage: ./ircserv <port> <password>");
-
-// 	Socket ss = Socket(PF_INET, SOCK_STREAM, 0);
-// 	ss.bind(check_port(av));
-// 	ss.listen();
-
-// 	vector<struct pollfd> vfds;
-// 	struct pollfd servpoll;
-// 	servpoll.fd = ss.getSock();
-// 	servpoll.events = POLLIN;
-// 	vfds.push_back(servpoll);
-
-// 	map<string, Client> clntList;
-	
-// 	while (1) {
-
-// 		if (poll(&vfds[0], vfds.size(), -1) == -1)
-// 			perr("Error: poll error");
-
-// 		if (vfds[0].revents & POLLIN) {
-// 			int clntfd = ss.accept();
-// 			cout << clntfd << "/client connected\n";
-// 			struct pollfd clntpoll;
-// 			clntpoll.fd = clntfd;
-// 			clntpoll.events = POLLIN;
-// 			vfds.push_back(clntpoll);
-// 			string msg = ss.recv(clntfd);
-// 			Client clnt = Client(clntfd);
-// 			Command cmd = Command(msg, clnt);
-// 			cmd.execute();
-// 			unsigned long before = clntList.size();
-// 			clntList.insert(make_pair(clnt.getNickname(), clnt));
-// 			if (before == clntList.size()) {
-// 				ss.send("same nickname exist.\n", clntfd);
-// 				vfds.pop_back();
-// 				close(clntfd);
-// 			}
-// 			else
-// 				ss.send("create client list.\n", clntfd);
-// 			print_map(clntList);
-// 		}
-
-// 		for (size_t i = 1; i < vfds.size(); i++) {
-// 			if (vfds[i].revents & POLLIN) {
-// 				int clntfd = vfds[i].fd;
-// 				string msg = ss.recv(clntfd);
-// 				if (msg.empty()) {
-// 					cout << "client end\n";
-// 					close(clntfd);
-// 					vfds.erase(vfds.begin() + i);
-// 					continue ;
-// 				}
-// 				// Command cmd(msg, clnt);
-// 				// cmd.printCommand();
-// 				ss.send(msg, clntfd);
-// 			}
-// 		}
-// 	}
-// 	return 0;
-// }
-
 int main(int ac, char **av)
 {
 	if (ac != 3) 
@@ -79,7 +15,6 @@ int main(int ac, char **av)
 
 	Socket ss = Socket(PF_INET, SOCK_STREAM, 0);
 	ss.bind(check_port(av));
-	fcntl(ss.getSock(), F_SETFL, O_NONBLOCK);
 	ss.listen();
 
 	vector<struct pollfd> vfds;
@@ -96,24 +31,16 @@ int main(int ac, char **av)
 			perr("Error: poll error");
 
 		if (vfds[0].revents & POLLIN) {
-			string 	msg;
-			int		clntfd = ss.accept();
-			Client 	clnt = Client(clntfd);
-
-			fcntl(clntfd, F_SETFL, O_NONBLOCK);
+			int clntfd = ss.accept();
 			cout << clntfd << "/client connected\n";
 			struct pollfd clntpoll;
 			clntpoll.fd = clntfd;
 			clntpoll.events = POLLIN;
 			vfds.push_back(clntpoll);
-			while (1) {
-				msg = ss.recv(clntfd);
-				if (errno == EAGAIN || errno == 
-				Command cmd = Command(msg, clnt);
-				cmd.execute();
-			}
-
-
+			string msg = ss.recv(clntfd);
+			Client clnt = Client(clntfd);
+			Command cmd = Command(msg, clnt);
+			cmd.execute();
 			unsigned long before = clntList.size();
 			clntList.insert(make_pair(clnt.getNickname(), clnt));
 			if (before == clntList.size()) {
@@ -144,3 +71,79 @@ int main(int ac, char **av)
 	}
 	return 0;
 }
+
+// int main(int ac, char **av)
+// {
+// 	if (ac != 3) 
+// 		perr("Usage: ./ircserv <port> <password>");
+
+// 	Socket ss = Socket(PF_INET, SOCK_STREAM, 0);
+// 	ss.bind(check_port(av));
+// 	fcntl(ss.getSock(), F_SETFL, O_NONBLOCK);
+// 	ss.listen();
+
+// 	vector<struct pollfd> vfds;
+// 	struct pollfd servpoll;
+// 	servpoll.fd = ss.getSock();
+// 	servpoll.events = POLLIN;
+// 	vfds.push_back(servpoll);
+
+// 	map<string, Client> clntList;
+	
+// 	while (1) {
+
+// 		if (poll(&vfds[0], vfds.size(), -1) == -1)
+// 			perr("Error: poll error");
+
+// 		if (vfds[0].revents & POLLIN) {
+// 			string 	msg;
+// 			int		clntfd = ss.accept();
+// 			Client 	clnt = Client(clntfd);
+
+// 			fcntl(clntfd, F_SETFL, O_NONBLOCK);
+// 			cout << clntfd << "/client connected\n";
+// 			struct pollfd clntpoll;
+// 			clntpoll.fd = clntfd;
+// 			clntpoll.events = POLLIN;
+// 			vfds.push_back(clntpoll);
+// 			while (1) {
+// 				msg = ss.recv(clntfd);
+// 				else if (errno == EAGAIN || errno == EWOULDBLOCK)
+// 					continue;
+// 				else {
+// 					Command cmd = Command(msg, clnt);
+// 					cmd.execute();
+// 				}
+// 			}
+
+
+// 			unsigned long before = clntList.size();
+// 			clntList.insert(make_pair(clnt.getNickname(), clnt));
+// 			if (before == clntList.size()) {
+// 				ss.send("same nickname exist.\n", clntfd);
+// 				vfds.pop_back();
+// 				close(clntfd);
+// 			}
+// 			else
+// 				ss.send("create client list.\n", clntfd);
+// 			print_map(clntList);
+// 		}
+
+// 		for (size_t i = 1; i < vfds.size(); i++) {
+// 			if (vfds[i].revents & POLLIN) {
+// 				int clntfd = vfds[i].fd;
+// 				string msg = ss.recv(clntfd);
+// 				if (msg.empty()) {
+// 					cout << "client end\n";
+// 					close(clntfd);
+// 					vfds.erase(vfds.begin() + i);
+// 					continue ;
+// 				}
+// 				// Command cmd(msg, clnt);
+// 				// cmd.printCommand();
+// 				ss.send(msg, clntfd);
+// 			}
+// 		}
+// 	}
+// 	return 0;
+// }
