84acf4f35930c42aa4025d4cde6e44cd01acfa03
yoonkiung <rldnd4429@gmail.com>
2023-05-16 22:09:11 +0900

    .

--- ft_irc/Command.cpp
+++ ft_irc/Command.cpp
@@ -217,24 +217,14 @@ void kick_channel(vector<Channel *> &channelList, string kick_channel) {
 		channelList.erase(channelList.begin() + i);
 }
 
-// int Command::findChannelIdx(string ch_name) {
-// 	for (int i = 0; i < (int)_chList.size(); i++) {
-// 		printf("[%s]\n", ch_name.c_str());
-// 		printf("[%s]\n", _chList[i]->getChannelName().c_str());
-// 		if (strcmp(ch_name.c_str(), _chList[i]->getChannelName().c_str()) == 0)
-// 			return i;
-// 	}
-// 	return -1;
-// }
 int Command::findChannelIdx(string ch_name) {
 	for (int i = 0; i < (int)_chList.size(); i++) {
-		printf("[%s]\n", ch_name.c_str());
-		printf("[%s]\n", _chList[i]->getChannelName().c_str());
-		if (ch_name == _chList[i]->getChannelName())
+		if (ch_name.c_str() == _chList[i]->getChannelName())
 			return i;
 	}
 	return -1;
 }
+
 string Command::kick(vector<string> token) {
 	int ch_idx;
 	
