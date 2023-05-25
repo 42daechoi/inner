0a8041f39da28455ec2ca3f0c125b08a7fe059f5
Daeyoung Choi <daechoi@c10r5s12.42seoul.kr>
2023-05-25 23:35:40 +0900

    superinvite

--- ft_irc/Command/superinvite.cpp
+++ ft_irc/Command/superinvite.cpp
@@ -11,7 +11,7 @@ void Command::superinvite(vector<string> token) {
 	for (int i = 0; i < (int)joinList.size(); i++) {
 		if (joinList[i]->isOperator(nickname)) {			
 			for (int j = 0; j < (int)_clntList.size(); j++) {
-				if (_clntList[j] == client || _clntList[j]->getNickname() != "bot")
+				if (_clntList[j] == client || _clntList[j]->getNickname() == "bot")
 					continue;
 				vector<string> temp;
 				temp.push_back("INVITE");
